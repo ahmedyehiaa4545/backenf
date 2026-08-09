cb42c1ccae6e735b030a25f2870000ead33bcda7
ahmedyehiaa4545 <ahmedyehiaa4545@gmail.com>
2026-08-09 05:00:59 +0300

    Add files via upload

--- remotion.config.ts
+++ remotion.config.ts
@@ -5,3 +5,6 @@ Config.setPixelFormat('yuv420p');
 
 // This ensures HTML elements are properly formatted and allows flexible CSS layout.
 Config.setAllowHtmlInCanvasEnabled(true);
+
+// Serve fonts from netlify-deploy folder (contains fonts/ subfolder with Thmanyah)
+Config.setPublicDir('netlify-deploy');
